95465cfdb63d08c7de523b80c7baf42c97b4cead
itc-n23030 <n23030@std.it-college.ac.jp>
2023-07-28 00:06:55 +0000

    4-7

--- 99.py
+++ 99.py
@@ -1,3 +1,6 @@
-n = [[i * n for i in range(1, 10)] for n in range(1, 10)]
-for i in n:
-    print(i)
+a = [(x, y, x * y) for x in range(1, 10) for y in range(1, 10)]
+s = ""
+for x, y, z in a:
+    s += "{:4d}".format(z)
+    if y == 9:
+        s += "\n"
